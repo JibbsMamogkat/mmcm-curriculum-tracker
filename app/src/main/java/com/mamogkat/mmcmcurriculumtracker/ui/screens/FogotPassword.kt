02f022938984868e6102c3e3cda0376bcc5c1a44
Duff Bastasa <drainbastasa1256@gmail.com>
2025-01-22 07:03:16 +0800

    Added ForgotPassword, Register UI and route in the Nav file, added link route in the LoginScreen and WhiteColor in Color Theme

---
+++ app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/FogotPassword.kt
@@ -0,0 +1,12 @@
+package com.mamogkat.mmcmcurriculumtracker.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.mamogkat.mmcmcurriculumtracker.navigation.Screen
+
+@Composable
+fun ForgotPassword(navController: NavController?) {
+    Text("Forgot Password Screen")
+}
