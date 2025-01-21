548650c8fb8910464aedaca7bb8f69ab4d988a65
Jameel <sbijjj1@gmail.com>
2025-01-21 20:27:55 +0800

    Initial commit

---
+++ app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/ChooseCurriculumScreen.kt
@@ -0,0 +1,10 @@
+package com.mamogkat.mmcmcurriculumtracker.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ChooseCurriculumScreen(navController: NavController) {
+    Text(text = "Choose Curriculum Screen")
+}
---
+++ app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/CurriculumOverviewScreen.kt
@@ -0,0 +1,10 @@
+package com.mamogkat.mmcmcurriculumtracker.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CurriculumOverviewScreen(navController: NavController) {
+    Text(text = "Curriculum Overview Screen")
+}
