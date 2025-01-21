f137f68bdad068c9fe37c3abeb8f8fbb76a9e56f
Jameel <sbijjj1@gmail.com>
2025-01-21 22:16:11 +0800

    Updated login screen, added mmcm logo and username, password and button

--- app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/LoginScreen.kt
@@ -1,10 +1,134 @@
 package com.mamogkat.mmcmcurriculumtracker.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.mamogkat.mmcmcurriculumtracker.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(navController: NavController) {
-    Text(text = "Login Screen")
+fun LoginScreen(navController: NavController?) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.mmcm_white)), // MMCM Red Background
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Logo
+            Image(
+                painter = painterResource(id = R.drawable.mmcm_logo),
+                contentDescription = "MMCM Logo",
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(bottom = 24.dp)
+            )
+
+            // Title Text
+            Text(
+                text = "Welcome to MMCM Tracker",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = MaterialTheme.typography.headlineLarge.fontStyle,
+                color = colorResource(id = R.color.mmcm_red), // MMCM Red
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            // Username Field
+            var username by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = colorResource(id = R.color.mmcm_white),
+                    focusedIndicatorColor = colorResource(id = R.color.mmcm_blue),
+                    unfocusedIndicatorColor = colorResource(id = R.color.mmcm_silver),
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+            )
+
+
+            // Password Field
+            var password by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = colorResource(id = R.color.mmcm_white),
+                    focusedIndicatorColor = colorResource(id = R.color.mmcm_blue),
+                    unfocusedIndicatorColor = colorResource(id = R.color.mmcm_silver),
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp)
+            )
+
+            // Login Button
+            Button(
+                onClick = { /* Handle Login */
+                            navController?.navigate("choose_curriculum")
+                          },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.mmcm_blue) // Delft Blue
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "Login",
+                    color = colorResource(id = R.color.mmcm_white),
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(navController = null)
 }
