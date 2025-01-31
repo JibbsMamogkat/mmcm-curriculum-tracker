d4bb49c9e3462c90872b51d44fb5c7c8f37815bb
Duff Bastasa <drainbastasa1256@gmail.com>
2025-02-01 00:14:16 +0800

    Added Program and Admin or User

--- app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/mamogkat/mmcmcurriculumtracker/ui/screens/RegisterScreen.kt
@@ -4,19 +4,31 @@ package com.mamogkat.mmcmcurriculumtracker.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.RadioButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -55,6 +67,7 @@ fun RegisterUI(navController: NavController?) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Logo
@@ -63,7 +76,6 @@ fun RegisterUI(navController: NavController?) {
                 contentDescription = "MMCM Logo",
                 modifier = Modifier
                     .size(150.dp)
-                    .padding(bottom = 24.dp)
             )
 
             // Title
@@ -73,41 +85,46 @@ fun RegisterUI(navController: NavController?) {
                 fontWeight = FontWeight.Bold,
                 fontStyle = MaterialTheme.typography.headlineLarge.fontStyle,
                 color = colorResource(id = R.color.mmcm_red),
-                modifier = Modifier.padding(bottom = 32.dp)
+                modifier = Modifier.offset(y = (-20).dp)
+
             )
 
             // Username
             var username by remember { mutableStateOf("") }
-
-            OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                label = { Text("Username") },
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 24.dp)
-                    .background(WhiteColor, RoundedCornerShape(8.dp))
-                    .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
-            )
-
             // Email
             var email by remember { mutableStateOf("") }
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("MMCM Email") },
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 24.dp)
-                    .background(WhiteColor, RoundedCornerShape(8.dp))
-                    .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
-            )
-
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = {
+                        if (it.length <= 10) {
+                            username = it
+                        }
+                    },
+                    label = { Text("Username") },
+                    singleLine = true,
+                    modifier = Modifier
+                        .weight(1f)
+                        .background(WhiteColor, RoundedCornerShape(8.dp))
+                        .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
+                        .padding(2.dp)
+                )
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("MMCM Email") },
+                    singleLine = true,
+                    modifier = Modifier
+                        .weight(1f)
+                        .background(WhiteColor, RoundedCornerShape(8.dp))
+                        .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
+                        .padding(2.dp)
+                )
+            }
             // Password
             var password by remember { mutableStateOf("") }
 
@@ -115,30 +132,59 @@ fun RegisterUI(navController: NavController?) {
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
+                singleLine = true,
                 visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 24.dp)
                     .background(WhiteColor, RoundedCornerShape(8.dp))
                     .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
+                    .padding(2.dp)
             )
 
             // Confirm password
             var confirmPassword by remember { mutableStateOf("") }
-
             OutlinedTextField(
                 value = confirmPassword,
                 onValueChange = { confirmPassword = it },
                 label = { Text("Confirm Password") },
+                singleLine = true,
                 visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 24.dp)
                     .background(WhiteColor, RoundedCornerShape(8.dp))
                     .border(1.dp, WhiteColor, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
+                    .padding(2.dp)
             )
+            // Admin or Student hehe
+            var selectedRole by remember { mutableStateOf("Student") }
+            ProgramDropdown(selectedRole)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.fillMaxWidth().padding(4.dp)
+            ) {
+                Text("Register as:", style = MaterialTheme.typography.bodyMedium)
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = selectedRole == "Admin",
+                        onClick = { selectedRole = "Admin" }
+                    )
+                    Text("Admin", modifier = Modifier.clickable { selectedRole = "Admin" })
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = selectedRole == "Student",
+                        onClick = { selectedRole = "Student" }
+                    )
+                    Text("Student", modifier = Modifier.clickable { selectedRole = "Student" })
+                }
+            }
             // Register
             Button(
                 onClick = {
@@ -157,7 +203,6 @@ fun RegisterUI(navController: NavController?) {
                     fontSize = 16.sp
                 )
             }
-            Spacer(modifier = Modifier.height(16.dp))
             // Back to Login
             Button(
                 onClick = {
@@ -175,6 +220,47 @@ fun RegisterUI(navController: NavController?) {
                     color = colorResource(id = R.color.mmcm_black),
                     fontSize = 16.sp
                 )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ProgramDropdown(selectedRole: String) {
+    val programList = listOf("BS Computer Engineering", "BS Electronics and Communications Engineering", "BS Electrical Engineering")
+    var selectedProgramList by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = selectedProgramList,
+            onValueChange = { },
+            readOnly = true,
+            enabled = selectedRole == "Student",
+            label = { Text("Program") },
+            trailingIcon = {
+                if(selectedRole == "Student") {
+                    IconButton(onClick = { expanded = !expanded }) {
+                        Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown")
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(enabled = selectedRole == "Student") { expanded = !expanded }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            programList.forEach { curriculum ->
+                DropdownMenuItem(
+                    text = {Text(text = curriculum )},
+                    onClick = {
+                        selectedProgramList = curriculum
+                        expanded = false
+                    })
             }
         }
     }
